08da18dce591ba35b42823f42a4e4767b74acfd6
Augusto Villar <augusto.villar@usp.br>
2023-07-19 14:11:53 -0300

    alteracao inter

--- arm-translate/32to64.js
+++ arm-translate/32to64.js
@@ -30,9 +30,11 @@ const instructions32to64 = {
 
 };
 
-var instructionsNumRegs = {
-    ADD: [2, 3],
-
+var instructionsRegs = {
+    ADD: [[1, 1, 1, 0, 1], [1, 1, 1, 0, 0], [1, 1, 1], [1, 1, 0]],
+};
+var instructionsImm = {
+    ADD: [[0, 0, 0, 0, 0], [0, 0, 0, 0, 1], [0, 0, 0], [0, 0, 1]],
 };
 
 function convert32to64(instructionSet){
@@ -45,27 +47,41 @@ function convert32to64(instructionSet){
         var instruction32 = vectorSplit[i].replace(/,/g,"").split(" "); //ADD r0, r1, r2
         var instruction64; 
         console.log(instruction32);
-
+        
         if (instruction32[0] in instructions32to64)
-        {
-            instruction64 = instructions32to64[instruction32[0]] + " "
-
-            console.log(instructionsNumRegs[instruction32[0]]);
-            var j = 0;
-            const regExRegistradores = "/r\b(?:1[0-5]|[0-9])\b/";
-
-            for(var i = 1; i<instruction32.length;i++)
-            {
-                //DOTO: nao deu certo a regex,arrumar 
-                if(regExRegistradores.test(instruction32[i]))
-                {
-                    j++;
-                    console.log(instruction32[i]);
-                }
-            }
+            instruction64 = instructions32to64[instruction32[0]] + " ";
+        else{
+            instruction64 = "Não existe"
+            break;
         }
 
-        instruction64 = "Não existe"
+        //verifica padrao de registradores e imediatos
+        var sequenciaRegs = [];
+        for(var i = 1; i<instruction32.length;i++)
+        {
+            if(/r([0-9]|1[0-5])/.test(instruction32[i]))
+                sequenciaRegs.push(1);
+            else
+                sequenciaRegs.push(0);
+        }
+        
+        //verifica qual é o item que tem correspondencia
+        console.log(instructionsRegs[instruction32[0]].length);
+        for(var i = 0; i<instructionsRegs[instruction32[0]].length; i++)
+        {
+            console.log(instructionsRegs[instruction32[0]][i]);
+            if(instructionsRegs[instruction32[0]][i].every((elemento, indice) => elemento === sequenciaRegs[indice])){                
+                var itemInstruction = i;
+                break;
+            }
+        }
+        console.log(itemInstruction);
+
+        var teste = instructionsRegs[instruction32[0]].some(elemento => JSON.stringify(elemento) === JSON.stringify(sequenciaRegs))
+
+        console.log(teste)
+
+        
         
 
 
